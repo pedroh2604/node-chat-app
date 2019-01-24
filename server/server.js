73b91ea408eea3bc4cafd36ffc1e5f5423884955
Pedro <pedrohenrique2604@gmail.com>
2019-01-24 11:52:00 -0200

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -11,9 +11,32 @@ var io = socketIO(server);
 
 app.use(express.static(PublicPath));
 
+// socking is listening when the connection is stablished server -> user
 io.on('connection', (socket) => {
+	// consoles when someone accesses localhost:3000
 	console.log('New user connect');
 
+	socket.emit('newEmail', {
+		from: 'peter@example.com',
+		text: 'Hey, whats going on',
+		createdAt: 123
+	});
+
+	// socket.on('createEmail', (newEmail) => {
+	// 	console.log('createEmail', newEmail);
+	// });
+
+	socket.emit('newMessage', {
+		from:'pete',
+		text: 'wow',
+		createdAt: 145
+	});
+
+	socket.on('createMessage', (message) => {
+		console.log('createMessage', message);
+	});
+
+	// consoles when someone closes the localhost:3000 tab
 	socket.on('disconnect', () => {
 		console.log('User was disconnected');
 	});
